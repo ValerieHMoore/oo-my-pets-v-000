0c541c1426503b2356e5e3546342defa0e6644de
Valerie Moore <periwinkle17@outlook.com>
2018-07-04 17:23:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :fishes, :dogs, :cats
+  attr_reader :species
+  
+  @@all = []
+  
+  #"is initialized with a pets attribute as a hash with 3 keys" 
+  #({:fishes => [], :dogs => [], :cats => []})
+  def initialize(species)
+    @species = species
+    @name = name
+    @fishes = :fishes => []
+    @dogs = :dogs => []
+    @cats = :cats => []
+    @pets = @fishes, @dogs, @cats
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+    "I am a human."
+  end
+
 end
